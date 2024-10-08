fa27bbe119dc3bbb4e1e94a5751276391c4010ae
aya <yokaosamaa640@gmail.com>
2024-10-08 20:31:08 +0300

    adding auth featcher

---
+++ lib/authintication/views/view/login_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', theme: ThemeData());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:chating_app/chat_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyChatApp());
+}
+
