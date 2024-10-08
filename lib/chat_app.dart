e36c5ca2d64f6a72ef814e07b3a0808c3dd6a03d
aya <yokaosamaa640@gmail.com>
2024-10-08 20:30:00 +0300

    add root of chat application

---
+++ lib/chat_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyChatApp extends StatelessWidget {
+  const MyChatApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', theme: ThemeData());
+  }
+}
+
